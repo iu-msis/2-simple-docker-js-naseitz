cf22ea81a41650c89465319bb7dbb2a392e63885
Nathan Seitz <nathanaseitz@gmail.com>
2020-09-18 15:48:58 -0400

    Implemented .catch(error) while fetchUser stayed functional

--- app/public/js/user.app.js
+++ app/public/js/user.app.js
@@ -27,14 +27,24 @@ var app = new Vue({
         this.picturelrg = userData.picture.large;
         this.picturesml = userData.picture.thumbnail;
         })
-      },
 
 
-
-
-      
-
-
-  }
-
+      .catch((error)=>{
+        console.error('error:',error);
+        fetch('.JSON/randomuser.me-sample.json')
+        .then(response => response.json())
+        .then(data => {
+          var userData = data.results[0];  //mayber UserInfo
+          console.log(userData);
+          this.username = userData.name.first + " " + userData.name.last;
+          this.email = userData.email;
+          this.country = userData.location.state;
+          this.birth = userData.dob.date[0] + userData.dob.date[1] + userData.dob.date[2] + userData.dob.date[3] + userData.dob.date[4] + userData.dob.date[5] + userData.dob.date[6] + userData.dob.date[7] + userData.dob.date[8] + userData.dob.date[9];
+          this.age = userData.dob.age;
+          this.picturelrg = userData.picture.large;
+          this.picturesml = userData.picture.thumbnail;
+        })
+      });
+ }
+}
 })
