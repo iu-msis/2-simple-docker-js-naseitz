834dc68fac02bbc1122f7cce4e424d9ef33f040e
Nathan Seitz <nathanaseitz@gmail.com>
2020-09-18 17:09:58 -0400

    Defined picturelrg and picturesml in data

--- app/public/js/user.app.js
+++ app/public/js/user.app.js
@@ -2,7 +2,8 @@ var app = new Vue({
   el: '#UserInfo', // Must have an object ID in user.html that matches this.
   data: {
     username: 'John Smith',
-    picture: 'picture',
+    picturelrg: 'large picture',
+    picturesml: 'small picture',
     country: 'country of origin',
     birth: 'birthdate',
     age: 'current age',
@@ -41,8 +42,8 @@ var app = new Vue({
           this.country = userData.location.state;
           this.birth = userData.dob.date[0] + userData.dob.date[1] + userData.dob.date[2] + userData.dob.date[3] + userData.dob.date[4] + userData.dob.date[5] + userData.dob.date[6] + userData.dob.date[7] + userData.dob.date[8] + userData.dob.date[9];
           this.age = userData.dob.age;
-          this.picturelrg = userData.picture.large;
-          this.picturesml = userData.picture.thumbnail;
+          //this.picturelrg = userData.picture.large;
+          //this.picturesml = userData.picture.thumbnail;
         })
       });
  }
