25d61210c54c28373c98327d9e0fe8389f03d016
Nathan Seitz <nathanaseitz@gmail.com>
2020-09-18 17:47:11 -0400

    Resolved console errors.  Done

--- app/public/js/user.app.js
+++ app/public/js/user.app.js
@@ -2,8 +2,8 @@ var app = new Vue({
   el: '#UserInfo', // Must have an object ID in user.html that matches this.
   data: {
     username: 'John Smith',
-    picturelrg: 'large picture',
-    picturesml: 'small picture',
+    picturelrg: '',
+    picturesml: '',
     country: 'country of origin',
     birth: 'birthdate',
     age: 'current age',
@@ -18,7 +18,7 @@ var app = new Vue({
         fetch('https://randomuser.me/api/')
         .then(response => response.json() )
         .then(data => {
-        var userData = data.results[0];  //mayber UserInfo
+        var userData = data.results[0];
         console.log(userData);
         this.username = userData.name.first + " " + userData.name.last;
         this.email = userData.email;
@@ -31,19 +31,19 @@ var app = new Vue({
 
 
       .catch((error)=>{
-        console.error('error:',error);
+        //console.error('error:',error);
         fetch('.JSON/randomuser.me-sample.json')
         .then(response => response.json())
         .then(data => {
-          var userData = data.results[0];  //mayber UserInfo
+          var userData = data.results[0];
           console.log(userData);
           this.username = userData.name.first + " " + userData.name.last;
           this.email = userData.email;
           this.country = userData.location.state;
           this.birth = userData.dob.date[0] + userData.dob.date[1] + userData.dob.date[2] + userData.dob.date[3] + userData.dob.date[4] + userData.dob.date[5] + userData.dob.date[6] + userData.dob.date[7] + userData.dob.date[8] + userData.dob.date[9];
           this.age = userData.dob.age;
-          //this.picturelrg = userData.picture.large;
-          //this.picturesml = userData.picture.thumbnail;
+          this.picturelrg = userData.picture.large;
+          this.picturesml = userData.picture.thumbnail;
         })
       });
  }
