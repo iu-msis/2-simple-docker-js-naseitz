be7cdf30901873bd3c4e7c308505bcc0ee047056
Nathan Seitz <nathanaseitz@gmail.com>
2020-10-19 19:51:34 -0400

    get and post
    Get works, post is almost there

--- app/public/api/comments/create_comment.php
+++ app/public/api/comments/create_comment.php
@@ -10,21 +10,20 @@ $db = DbConnection::getConnection();
 // Step 2: Create & run the query
 // Note the use of parameterized statements to avoid injection
 $stmt = $db->prepare(
-  'INSERT INTO Comments (id, commentText)
+  'INSERT INTO Comments (commentText)
   VALUES
-  ( ?, ?)'
+  (?)'
 );
 
 $stmt->execute([
-  $_POST['id'],
   $_POST['commentText'],
 ]);
 
 // If needed, get auto-generated PK from DB
-// $pk = $db->lastInsertId();  // https://www.php.net/manual/en/pdo.lastinsertid.php
+$pk = $db->lastInsertId();  // https://www.php.net/manual/en/pdo.lastinsertid.php
 
 // Step 4: Output
 // Here, instead of giving output, I'm redirecting to the SELECT API,
 // just in case the data changed by entering it
 header('HTTP/1.1 303 See Other');
-header('Location: ../comments/?id=' . $id);
+header('Location: ../comments/?id=' . $pk);
